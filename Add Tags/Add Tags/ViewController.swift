d8be96676aa25cab3cbadddf9fa3e22fbada694a
Daniil Sikidin <dansqr2@gmail.com>
2023-08-01 15:56:52 -0700

    init

---
+++ Add Tags/Add Tags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Add Tags
+//
+//  Created by dansqr3 on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
